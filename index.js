b1f1188876481fb012658d33245ac129b588d722
xubaoshi <948869516@qq.com>
2019-11-20 21:52:34 +0800

    init project

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 var express = require('express')
+var errorHandler = require('errorHandler')
+var config = require('./config')
 
 var app = express()
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(errorHandler())
+} else {
+  app.use
+}
+
+app.listen(config.port, function() {
+  console.log('Express server listening on %d')
+})
