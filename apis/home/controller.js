fb67aa97be46f658348426ad64569cbb6ed43bbb
xubaoshi <948869516@qq.com>
2019-11-24 13:56:41 +0800

    edit code

--- apis/home/controller.js
+++ apis/home/controller.js
@@ -25,22 +25,42 @@ exports.getHome = asyncHandler(async function(req, res) {
     // content
     var contentClass = '.li_l a'
 
-    topics.forEach(async topic => {
+    topics.forEach(async (topic, index) => {
       var title = await topic.$$(titieClass)
-      var content = await  topic.$$(contentClass)
+      var content = await topic.$$(contentClass)
       var contentData = []
-      console.log(title)
-      console.log(content)
-      if (commonUtil.isNotEmpty(contentData)) {
+
+      // title
+      if (commonUtil.isNotEmpty(title)) {
+        var titNameHandle = await title[0].getProperty('textContent')
+        var titName = await titNameHandle.jsonValue()
+        var titHandleLinkHandle = await title.getProperty('href')
+        var titLink = await titHandleLinkHandle.jsonValue()
+      }
+
+      // content
+      if (commonUtil.isNotEmpty(content)) {
         content.forEach(async item => {
-          var itemName = item.innerHtml
-          var itemLink = await item.getProperties('href')
-          console.log(itemName)
+          var itemNameHandle = await item.getProperty('textContent')
+          var itemName = await itemNameHandle.jsonValue()
+          var itemLinkHandle = await item.getProperty('href')
+          var itemLink = await itemLinkHandle.jsonValue()
+          contentData.push({
+            title: itemName,
+            path: itemLink
+          })
         })
+      }
+
+      topicsData.push({
+        title: titName,
+        moreLInk: titLink,
+        content: contentData
+      })
+
+      if (index === topics.length - 1) {
+        res.send(topicsData)
       }
     })
   }
-
-  console.log(topics)
-  res.send('hello')
 })
